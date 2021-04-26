7bb3d65a35fb27caf5b5cf059bc493b0c7a76aa6
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2021-04-26 12:44:39 +1000

    fixed inputs

--- src/pages/Task_creation.tsx
+++ src/pages/Task_creation.tsx
@@ -15,46 +15,80 @@ interface ITaskData {
     name: string,
     desc: string,
     [index: number]: { day: string };
-    mesure: string,
+    mesure: string | any,
 
 }
 const Task: React.FC = () => {
-    var newTask: ITaskData = {
+    
+    const [newTask, setNewTask] = useState<ITaskData>({
         name: '',
         desc: '',
         daysOfWeek: [],
         mesure: 'timer'
-    }
-    var dotwCheckboxList = [
-        { id: 'mon', label: 'Monday' },
-        { id: 'tue', label: 'Tuesday' },
-        { id: 'wen', label: 'Wensday' },
-        { id: 'thur', label: 'Thursday' },
-        { id: 'fri', label: 'Friday' },
-        { id: 'sat', label: 'Saturday' },
-        { id: 'sun', label: 'Sunday' },
-    ];
+    })
+
+    const [dotwCheckboxList, setDotwCheckboxList] = useState([
+        { id: 'mon', label: 'Monday', disabled: false },
+        { id: 'tue', label: 'Tuesday', disabled: false },
+        { id: 'wen', label: 'Wensday', disabled: false },
+        { id: 'thur', label: 'Thursday', disabled: false },
+        { id: 'fri', label: 'Friday', disabled: false },
+        { id: 'sat', label: 'Saturday', disabled: false },
+        { id: 'sun', label: 'Sunday', disabled: false },
+        { id: 'all', label: 'Every Day', disabled: false },
+    ]);
 
     const [dotwIdMapping, setDotwIdMapping] = useState<EuiCheckboxGroupIdToSelectedMap>({});
 
-    const onDotwChange = (optionId: keyof object | string) => {
-        const newCheckboxIdToSelectedMap = {
-            ...dotwIdMapping,
-            ...{
-                [optionId]: !dotwIdMapping[optionId],
-            },
-        };
-        setDotwIdMapping(newCheckboxIdToSelectedMap)
 
+    const onDotwChange = (optionId: keyof object | string) => {
+        //disable all other options and set them unchecked
+        if (optionId == "all") {
+
+            var newSelectedMap = dotwIdMapping;
+            const toDisabled = !dotwIdMapping['all']
+
+            const updatedDotwList = dotwCheckboxList.map((item) => {
+                var updatedItem = item
+                if (item.id != 'all') {
+                    updatedItem.disabled = toDisabled
+                    newSelectedMap[item.id] = false
+                } else {
+                    newSelectedMap['all'] = !dotwIdMapping['all']
+                }
+                return updatedItem
+            })
+            setDotwCheckboxList(updatedDotwList)
+            setDotwIdMapping(newSelectedMap)
+
+        } else {
+            const newCheckboxIdToSelectedMap = {
+                ...dotwIdMapping,
+                ...{
+                    [optionId]: !dotwIdMapping[optionId],
+                },
+            };
+            setDotwIdMapping(newCheckboxIdToSelectedMap)
+        }
     };
+
     useEffect(() => {
-        newTask['daysOfWeek'] = Object.keys(dotwIdMapping).filter(key => dotwIdMapping[key] == true);
-    },[dotwIdMapping])
+        //set the item value when dotw changes
+        if (Object.keys(dotwIdMapping).find(() => dotwIdMapping['all'] != true)) {
+            updateTaskValue('daysOfWeek', Object.keys(dotwIdMapping).filter(key => dotwIdMapping[key] == true))
+        } else {
+            updateTaskValue('daysOfWeek',Object.values(dotwCheckboxList).map((item) => {
+                if (item.id != "all") {
+                    return item.id
+                }
+            }))
+        }
+    }, [dotwCheckboxList, dotwIdMapping])
 
     var taskMesureOptions = [
         {
             id: 'timer',
-            label: 'Timer'
+            label: 'Timer',
         },
         {
             id: 'counter',
@@ -70,19 +104,31 @@ const Task: React.FC = () => {
     const [taskMesureIdMapping, setTaskMesureIdMapping] = useState<EuiCheckboxGroupIdToSelectedMap>(taskMesureIdMapDefault);
 
     const onTaskMesureChange = (optionId: keyof object | string) => {
-        setTaskMesureIdMapping(taskMesureIdMapDefault);
-        const newTaskMesureIdMap = {
-            ...taskMesureIdMapDefault,
-            ...{
-                [optionId]: !taskMesureIdMapping[optionId],
-            },
+        var newTaskMesureIdMap: EuiCheckboxGroupIdToSelectedMap = {}
+        if (optionId == "timer") {
+            newTaskMesureIdMap = {
+                timer: true,
+                counter: false
+            }
+        } else {
+            newTaskMesureIdMap = {
+                timer: false,
+                counter: true
+            }
+        }
 
-        };
+        updateTaskValue('mesure', optionId)
         setTaskMesureIdMapping(newTaskMesureIdMap);
     };
 
-    function updateTaskValue(key: string, value: string) {
-        newTask[key] = value
+    useEffect(() => {
+        updateTaskValue('mesure', Object.keys(taskMesureIdMapping).find((item) => taskMesureIdMapping[item] == true))
+    },[taskMesureIdMapping])
+
+    function updateTaskValue(key: string, value: string | any) {
+        const updatedTask = newTask
+        updatedTask[key] = value
+        setNewTask(updatedTask)
     }
 
     function createTask() {
@@ -103,19 +149,20 @@ const Task: React.FC = () => {
                 <EuiFormRow label="Task name">
                     <EuiFieldText
                         name="name"
-                        onBlur={(event) => updateTaskValue(event.currentTarget.name, event.currentTarget.value)}
+                        onChange={(event) => updateTaskValue(event.currentTarget.name, event.currentTarget.value)}
                     />
                 </EuiFormRow>
 
                 <EuiFormRow label="Task Description">
                     <EuiTextArea
                         name="desc"
-                        onBlur={(event) => updateTaskValue(event.currentTarget.name, event.currentTarget.value)}
+                        onChange={(event) => updateTaskValue(event.currentTarget.name, event.currentTarget.value)}
                     />
                 </EuiFormRow>
 
                 <EuiFormRow label="Days of the Week">
                     <EuiCheckboxGroup
+
                         options={dotwCheckboxList}
                         idToSelectedMap={dotwIdMapping}
                         onChange={(id) => onDotwChange(id)}
@@ -129,6 +176,7 @@ const Task: React.FC = () => {
                         onChange={(id) => onTaskMesureChange(id)}
                     />
                 </EuiFormRow>
+
 
                 <EuiButton fill onClick={() => createTask()}>
                     Create
