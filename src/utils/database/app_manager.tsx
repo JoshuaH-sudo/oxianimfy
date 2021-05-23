02bbb43af42bfbcbb58257571a3469fc0f1be51a
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2021-05-23 16:36:16 +1000

    clean up

---
+++ src/utils/database/app_manager.tsx
@@ -0,0 +1,48 @@
+import { Database } from './database';
+import { Storage } from '@ionic/storage';
+import { ISettings } from '../custom_types';
+import moment from 'moment';
+
+export class App_manager {
+    storeName: string = 'settings'
+    store: Storage;
+
+    settings: ISettings = {
+        selectedTaskGroup: '',
+        lastRefreshTimeStamp: moment().startOf('day').toString()
+    }
+
+    constructor(app_database: Database) {
+        this.store = app_database.store
+        this.store.get(this.storeName).then((result: any) => {
+            console.log(this.storeName, result)
+        })
+    }
+
+    createConfig = async () => {
+        const keys = await this.store.keys()
+        if (keys.find(key => key == this.storeName) == undefined) {
+            await this.store.set(this.storeName, this.settings)
+        }
+    }
+
+    getConfig = async () => {
+        return await this.store.get(this.storeName) ?? this.settings;
+    }
+
+    updateTimeStamp = async () => {
+        let updatedSettings: ISettings = await this.store.get(this.storeName) ?? this.settings;
+        updatedSettings.lastRefreshTimeStamp = moment().startOf('day').toString()
+    }
+
+    setSelectedTaskGroup = async (taskSetId: string) => {
+        let updatedSettings: ISettings = await this.store.get(this.storeName) ?? this.settings;
+        updatedSettings.selectedTaskGroup = taskSetId
+        return await this.store.set(this.storeName, updatedSettings)
+    }
+
+    getSelectedTaskGroup = async () => {
+        let updatedSettings: ISettings = await this.store.get(this.storeName) ?? this.settings;
+        return updatedSettings.selectedTaskGroup
+    }
+}
