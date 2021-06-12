376ba8a8f16701d109e7938b410721e6eb6ff418
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2021-06-13 07:44:31 +1000

    Revert "Stats (#4)"
    This reverts commit 6afa2a82935a1d02682903b542d50ba94a6f9372.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import Play from "./pages/Play";
-import Task from "./pages/Task_creation";
-import Edit_task_menu from "./pages/Edit_task_menu";
-import { Stats } from "./pages/Stats_display";
-import Task_selection from "./pages/Task_selection";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Play from './pages/Play';
+import Task from './pages/Task_creation';
+import Edit_task_menu from './pages/Edit_task_menu';
 import { HashRouter, Route, Switch } from "react-router-dom";
-import { EuiPanel, EuiButton } from "@elastic/eui";
-import { Nav_bar } from "./components/Nav_bar";
+import Task_selection from './pages/Task_selection';
+import {
+  EuiPanel,
+  EuiButton
+} from '@elastic/eui';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Nav_bar />
     <EuiPanel>
+      <EuiButton fill href="#/">Home</EuiButton>
       <HashRouter>
         <Switch>
           <Route path="/" exact component={App} />
@@ -22,10 +22,10 @@ ReactDOM.render(
           <Route path="/task_selection" component={Task_selection} />
           <Route path="/play" component={Play} />
           <Route path="/edit-tasks" component={Edit_task_menu} />
-          <Route path="/stats" component={Stats} />
         </Switch>
       </HashRouter>
     </EuiPanel>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
--- src/utils/custom_types.ts
+++ src/utils/custom_types.ts
@@ -1,42 +1,38 @@
 export interface ITaskData {
-  [k: string]: any;
-  name: string;
-  desc: string;
-  [index: number]: { day: string };
-  mesure: string | any;
-  unit: number | string;
+    [k: string]: any,
+    name: string,
+    desc: string,
+    [index: number]: { day: string };
+    mesure: string | any,
+    unit: number | string
+
 }
 
 export interface setRef {
-  [k: string]: any;
-  name: string;
-  desc: string;
-  [index: number]: { taskId: string; completed: boolean };
+    [k: string]: any,
+    name: string,
+    desc: string,
+    [index: number]: { taskId: string, completed: boolean };
 }
 
 export interface ISetData {
-  [index: string]: any | setRef;
+    [index: string]: any | setRef,
 }
 
 export interface ITaskSetData {
-  [index: string]: any | ITaskData;
+    [index: string]: any | ITaskData,
 }
 
 export interface taskRef {
-  taskId: string;
-  completed: boolean;
-}
-
-export interface streakData {
-  id: string;
-  streak: number;
+    taskId: string,
+    completed: boolean
 }
 
 export interface IScheduleData {
-  [index: string]: any | taskRef;
+    [index: string]: any | taskRef,
 }
 
 export interface ISettings {
-  [k: string]: any;
-  selectedTaskGroup: string;
+    [k: string]: any,
+    selectedTaskGroup: string
 }
