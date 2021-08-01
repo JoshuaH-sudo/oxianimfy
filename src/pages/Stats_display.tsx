bb63457b7909f45f66ef99764a90f64d7c40c3ef
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2021-08-02 08:35:09 +1000

    pushing changes

--- src/pages/Stats_display.tsx
+++ src/pages/Stats_display.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { EuiPanel, EuiTabs, EuiTab, EuiSpacer } from "@elastic/eui";
 import {
   Set_streak_stats,
@@ -8,29 +8,30 @@ import {
 
 export const Stats: React.FC = () => {
   const [current_stat_view, set_current_stat_view] =
-    useState<string>("set_streak");
+    useState<string>("week_workload");
 
   const display = () => {
     switch (current_stat_view) {
-      case "set_streak":
-        return <Set_streak_stats />;
       case "week_workload":
-        return <Week_workload />;
+        return <Week_workload />
       case "app_stats":
-        return <App_stats />;
+        return (
+          <Fragment>
+            <Set_streak_stats />
+            <App_stats />
+          </Fragment>
+        );
     }
   };
   return (
     <EuiPanel paddingSize="l">
       <EuiTabs display="condensed">
-        <EuiTab onClick={() => set_current_stat_view("set_streak")}>
-          Set Streaks
-        </EuiTab>
         <EuiTab onClick={() => set_current_stat_view("week_workload")}>
           Week Workload
         </EuiTab>
+
         <EuiTab onClick={() => set_current_stat_view("app_stats")}>
-          App Stats
+          Stats
         </EuiTab>
       </EuiTabs>
 
